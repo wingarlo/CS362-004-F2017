9e82b74ea7b2bae5c82b8d1b76a5cd28345c5153
Logan Wingard <wingarlo@os2.engr.oregonstate.edu>
2017-10-20 18:01:19 -0700

    first card test added (smithy)

---
+++ projects/wingarlo/dominion/cardtest1.c
@@ -0,0 +1,65 @@
+/***
+ * Logan Wingard
+ * 10/20/2017
+ * cardtest1.c
+ *
+ * This tests the function smith(struct gameState *state, int currentPlayer, int handPos)
+ *
+ * Include the following lines in your makefile:
+ *
+ * cardtest1: cardtest1.c dominion.o rngs.o
+ *      gcc -o cardtest1 -g  cardtest1.c dominion.o rngs.o $(CFLAGS)
+ */
+
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+#define TESTCARD "smithy"
+
+int main() {
+    int newCards = 0;
+    int discarded = 1;
+    int shuffledCards = 0;
+
+    int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
+    int remove1, remove2;
+    int seed = 1000;
+    int numPlayers = 2;
+    int thisPlayer = 0;
+	struct gameState G, testG;
+	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+			sea_hag, tribute, smithy, council_room};
+
+	// initialize a game state and player cards
+	initializeGame(numPlayers, k, seed, &G);
+
+	printf("----------------- Testing Card: %s ----------------\n", TESTCARD);
+
+	// ----------- TEST 1: choice1 = 1 = +3 cards --------------
+	printf("TEST 1: choice1 = 1 = +3 cards\n");
+
+	// copy the game state to a test case
+	memcpy(&testG, &G, sizeof(struct gameState));
+	choice1 = 1;
+	smith(&testG, thisPlayer, handpos);
+
+	newCards = 3;
+	printf("hand count = %d, expected = %d\n", testG.handCount[thisPlayer], G.handCount[thisPlayer] + newCards - discarded);
+	printf("deck count = %d, expected = %d\n", testG.deckCount[thisPlayer], G.deckCount[thisPlayer] - newCards + shuffledCards);
+	assert(testG.handCount[thisPlayer] == G.handCount[thisPlayer] + newCards - discarded);
+	assert(testG.deckCount[thisPlayer] == G.deckCount[thisPlayer] - newCards + shuffledCards);
+
+
+	printf("\n >>>>> SUCCESS: Testing complete %s <<<<<\n\n", TESTCARD);
+
+
+	return 0;
+}
+
+
